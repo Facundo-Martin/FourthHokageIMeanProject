2cca5025786670ccbf49d01a5e997fdef11527d2
Facundo Martin <facundomartin98@gmail.com>
2021-12-16 11:04:29 -0500

    final commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== HOME SWIPER ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
